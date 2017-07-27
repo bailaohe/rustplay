759546a3e5c38035dd09680cb3a81b5ee6ef60f4
bailaohe <bailaohe@gmail.com>
2017-07-27 21:29:54 +0800

    init

---
+++ argparser/src/main.rs
@@ -0,0 +1,29 @@
+// #![cfg_attr(feature="clippy", feature(plugin))]
+#![feature(plugin)]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+extern crate clap;
+
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("DemoApp")
+        .long_version("1.0")
+        .author("baihe")
+        .about("demo app to indicate use of clap")
+        .arg(Arg::with_name("arg1")
+            .short("A")
+            .long("arg1")
+            .value_name("ARG1")
+            .required(true)
+            .help("the first argument")
+            .takes_value(true))
+        .get_matches();
+
+    let val1 = match matches.value_of("arg1") {
+        Some(v) => v,
+        None => "default_value"
+    };
+
+    println!("{}", val1);
+}
